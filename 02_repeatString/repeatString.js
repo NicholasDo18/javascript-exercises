243adb306dc1bb0e9d170a88a156da446c4706f2
NicholasDo18 <146508658+NicholasDo18@users.noreply.github.com.>
2025-04-09 23:36:12 -0400

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = "";
 
+    while(num>0){
+        finalString +=string;
+
+        num--;
+
+    }
+    return finalString;
 };
 
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
