7bdb44a7fae8a5ad496f2a34ccebc40eeb80aa64
NicholasDo18 <146508658+NicholasDo18@users.noreply.github.com.>
2025-04-12 00:06:16 -0400

    completed reverse string task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split(""); //makes the string to an array.
+    let reverseArray = splitString.reverse(); //reverses the order of the array.
+    let reverseStr = reverseArray.join("");
+    return reverseStr;
 
 };
 
+reverseString('hello there');
 // Do not edit below this line
 module.exports = reverseString;
