bc922e2490df901ad424eba6c82b64f430fb37b0
NicholasDo18 <146508658+NicholasDo18@users.noreply.github.com.>
2025-04-12 00:26:09 -0400

    completed the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, number) {
+    for (let i = 0; i< array.length;i++){
+        if (array[i] == number){
+            array.splice(i,1);
+        }else{
+            continue;
+        }
+
+    }
+    return array;
 };
 
+removeFromArray([1,2,3,4],3);
 // Do not edit below this line
 module.exports = removeFromArray;
